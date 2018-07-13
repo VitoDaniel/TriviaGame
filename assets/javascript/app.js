48a72f19d0236ad05c3f58083bd92372c1f97d25
Vito Daniel <vitodaniel93@gmail.com>
2018-07-13 11:56:20 -0400

    got the timer working, no logic yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -48,30 +48,46 @@ $(document).ready(function() {
     var wrong = 0;
     // unanswered
     var none = 0; 
-    // set our timer to 90
-    var count = 90;
+  
 
     // ============================================= functions:
 
+
+    
+    var timer = 90;
+    var timer;
+    var isTimeRunning = false;
+
+    function startTimer(){
+        if(!isTimeRunning) {
+            time = setInterval(countDown, 1000);
+            isTimeRunning = true;
+        }
+    }
+        
+    function countDown(){
+            $("#timer").html(timer);
+            timer--;
+            console.log(timer);
+            if(timer === 0) {
+                none++;
+                stopCount();
+            }
+    }
+
+    function stopCount(){
+            isTimeRunning=false;
+            clearInterval(time);
+    }
+
+
     // start the game onse button start is pressed
     $("#start").on("click", function(){
-        $("#start").fadeOut(500, displayQ);
+        $("#start").fadeOut(500,displayQ, startTimer());
     });
 
     function displayQ(){
 
-    
-
-        // var counter = setInterval(timer, 1000);
-        // function timer(){
-        //     count--;
-        //     if (count<=0){
-        //         clearInterval(counter);
-        //         // return; will add later
-        //     }
-        // }
-
-        // $("#questionArea").append(count);
 
         for (i=0; i < myQuestions.length; i++) {
             var  questionDiv = $("<div>").addClass("qContainer");
@@ -80,154 +96,17 @@ $(document).ready(function() {
         
 
             for (j=0; j < myQuestions[i].answers.length; j++){
-                var answer = $("<input type='radio' name ='question-" + i + "' >").addClass("answer").text(myQuestions[i].answers[j]);
+                var answer = $("<h4> <input type='radio' name = 'question - " + i + "' >").addClass("answer").text(myQuestions[i].answers[j]);
                 questionDiv.append(answer);
             }
         $("#questionArea").append(questionDiv);
         
         
 
-
         }
     }
 
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     // function to display question and remove previous content
-//     function displayQ(){
-//         $(".message-content").remove();
-//         $("#start").remove();
-//     }
-
-//     // create variables for timer, quesiton and question and give it value of id in html
-//    var timer =  $("#timer");
-//    var question = $("#question");
-//    var questionArea = $("#questionArea");
-
-//     //    // append the elements so they display properly
-//     //    timer.appendTo(questionArea);
-//     //    question.appendTo(questionArea);
-
-
-//     // set up countdown timer
-//     var time = 30;
-//     timer.html("#timer" + time + "seconds remaining.");
-
-//     // countdown function that stops when time==0
-//     var countDown = setInterval(function(){
-//         time--;
-//         timer.html("#timer" + time + "seconds remaining.");
-
-//         if (time === 0){
-//             clearInterval(countDown);
-//             questionArea.fadeToggle("slow", timedOut);
-//             none++;
-//         }
-//     }, 1000);
-
-
-
-    // var triviaContainer = document.getElementById('trivia');
-    // var resultsContainier = document.getElementById('results');
-    // var submitButton = document.getElementById('submit');
-    
-    // generateTrivia (myQuestions, triviaContainer, resultsContainier, submitButton);
-    
-    
-    
-    // function generateTrivia (questions, triviaContainer, resultsContainier, submitButton){
-        
-    //     function showQuestion(questions, triviaContainer){
-    //         // store output and answer choises
-    //         var output = [];
-    //         var answers;
-
-    //         // for each question:
-    //         for(var i = 0; i<questions.length; i++){
-
-    //             // reset the list of answers
-    //             answers = [];
-
-    //             // for each available answer to this question add an html radio button
-    //             for(letter in questions[i].answers){
-    //                 answer.push(
-    //                     '<label>'
-    //                       +'<input type = "radio" name = "question '+i+'" value="'+letter+'">'
-    //                       + letter + ':'
-    //                       + questions[i].answers[letter]
-    //                     + '</label>'
-    //                 );
-    //             }
-
-    //             // add this questions and its anwers to the output
-    //             output.push(
-    //                 '<div class = "question">' + questions[i].question + '</div>'
-    //                 + '<div class = "answers">' + answers.join('') + '</div>'
-    //             );
-    //         }
-
-    //         // combine our output list into one string of html and siplay on a page
-    //         triviaContainer.innerHTML = ouput.join('');
-    //     }
-
-    //     function showResults(questions, triviaContainer, resultsContainier){
-    //          var answerContainers = triviaContainer.querySelectorAll('.answers');
-
-    //          var userAnswer = '';
-    //          var numCorrect = 0;
-
-    //          for(var i=0; i<questions.length; i++){
-    //              userAnswer = (answerContainers[i].querySelectorAll('input[name=question'+i+']:checked;')||{}).value;
-
-    //              if(userAnswer === questions[i].correctAnswer){
-    //                  numCorrect++;
-
-    //                  answerContainers[i].style.color = 'lighgreen';
-    //              } else {
-    //                  answerContainers[i].style.color = 'red';
-    //              }
-    //          }
-
-    //          resultsContainier.innerHTML = numCorrect + ' out of ' + questions.length;
-    //     }
-    //     // show the questions
-    //     showQuestion(questions, triviaContainer);
-
-    //     // when user clicks submit, show results
-    //     submitButton.onclick = function(){
-    //         showResults(questions, triviaContainer, resultsContainier);
-    //     }
-    // }
-
-    
-
-    
-
-
- 
-
 
 
 });
